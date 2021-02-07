7f1f7d0cae40718db2fca735a1e56fedfbaf4a80
TheAnswerIs43 <56391254+TheAnswerIs43@users.noreply.github.com>
2021-02-07 22:25:25 +0100

    Add files via upload

--- myflopslib/myfunction.py
+++ myflopslib/myfunction.py
@@ -1,5 +1,5 @@
 import numpy as np
-from keras import layers
+from tensorflow.keras import layers
 
 
 def default(x:layers.Layer):
@@ -23,9 +23,9 @@ def compute_act(x:layers.Layer):
 def dense(x:layers.Layer):
   inp = np.prod(x.input_shape[1:])
   out = np.prod(x.output_shape[1:])
-  flops = inp*out*2
+  flops = inp*out
   flops += compute_act(x)
-  return flops
+  return 2*flops
     
 def zero(x:layers.Layer):
   flops = 0
@@ -35,17 +35,37 @@ def zero(x:layers.Layer):
 def conv(x:layers.Layer):
   out = np.prod(x.output_shape[1:])
   kernel_size = np.prod(x.kernel_size)
-  flops = 2 * out * (x.input_shape[-1] * kernel_size)
+  flops = out * (x.input_shape[-1] * kernel_size)
   flops += compute_act(x)
-  return flops
+  return 2*flops
 
 def conv_transpose(x:layers.Layer):
   out = np.prod(x.input_shape[1:])
   kernel_size = np.prod(x.kernel_size)
-  flops = 2 * out * (x.output_shape[-1] * kernel_size)
+  flops = out * (x.output_shape[-1] * kernel_size)
   flops += compute_act(x)
-  return flops
+  return 2*flops
 
 def batch_norm(x:layers.Layer):
   flops = np.prod(x.input_shape[1:])
-  return flops*2
+  return 2*flops
+
+def separable_conv(x:layers.Layer):
+  first_out_channel = x.input_shape[-1] * x.depth_multiplier
+  single_channel = x.output_shape[1] * x.output_shape[2]
+  first_out = first_out_channel * single_channel
+  first_kernel_size = np.prod(x.kernel_size)
+  first_flops = first_out * first_kernel_size
+  out = np.prod(x.output_shape[1:])
+  kernel_size = x.input_shape[-1]*x.depth_multiplier
+  flops = out * kernel_size
+  return 2*(first_flops + flops)
+
+
+def depthwise_conv(x:layers.Layer):
+  out = np.prod(x.output_shape[1:])
+  kernel_size = np.prod(x.kernel_size)
+  flops = out * kernel_size
+  return 2*flops
+
+
--- myflopslib/profiler.py
+++ myflopslib/profiler.py
@@ -1,7 +1,7 @@
 import numpy as np
-import keras
-from keras import layers 
-from myflopslib.myfunction import *
+from tensorflow.keras import models
+from tensorflow.keras import layers 
+from myflopslib.myfunction import dense, conv, conv_transpose, batch_norm, zero, separable_conv, depthwise_conv
 from prettytable import PrettyTable
 
 
@@ -12,6 +12,8 @@ mydict = {
     "Conv2D":conv,
     "Conv2DTranspose":conv_transpose,
     "BatchNormalization": batch_norm,
+    "SeparableConv2D": separable_conv,
+    "DepthwiseConv2D": depthwise_conv,
     "ReLU": zero
 }
 
@@ -23,20 +25,26 @@ class Profiler:
         self.table = None
 
 
-    def counter(self, mod: keras.models.Model, flag = True):
-        if not isinstance(mod, keras.models.Model):
+    def counter(self, mod: models.Model, flag = True):
+        if not isinstance(mod, models.Model):
             print("Input Error")
             return 0
         for layer in mod.layers[1:]:
             key = layer.__class__.__name__
             if key in mydict.keys():
                 ops = mydict[key](layer)
-                self.table.add_row([key, layer.input_shape, layer.output_shape, ops])
+                input_size = "none"
+                output_size = "none"
+                if(hasattr(layer, "input_shape")):
+                    input_size = layer.input_shape
+                if(hasattr(layer, "output_shape")):
+                    output_size = layer.output_shape
+                self.table.add_row([key, input_size, output_size, ops])
                 self.flops += ops
             elif hasattr(layer, "layers"):
                 self.counter(layer, False)
             else:
-                self.table.add_row([key,layer.input_shape,layer.output_shape,"Not Implemented"])
+                self.table.add_row([key,"Not Implemented","Not Implemented","Not Implemented"])
  
         if bool(flag):
             print(self.table)
@@ -44,7 +52,7 @@ class Profiler:
         
         return 0
     
-    def compute_flops(self, mod: keras.models.Model):
+    def compute_flops(self, mod: models.Model):
         self.flops = 0
         self.table = PrettyTable(default_header)
         self.counter(mod, 1)
