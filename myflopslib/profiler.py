593871ca02d0474f566ce904e7d407b7e4623eef
TheAnswerIs43 <56391254+TheAnswerIs43@users.noreply.github.com>
2021-02-10 03:43:28 +0100

    Add files via upload

--- myflopslib/profiler.py
+++ myflopslib/profiler.py
@@ -1,19 +1,22 @@
 import numpy as np
 from tensorflow.keras import models
 from tensorflow.keras import layers 
-from myflopslib.myfunction import dense, conv, conv_transpose, batch_norm, zero, separable_conv, depthwise_conv
+from myflopslib.myfunction import dense, conv, conv_transpose, batch_norm, zero, separable_conv, depthwise_conv, compute_act
 from prettytable import PrettyTable
 
 
-default_header = ["Layer", "Name", "Input Shape", "Output Shape", "FLOPs"]
+default_header = ["Layer", "Name","Input Shape", "Output Shape", "FLOPs"]
 
 mydict = {
     "Dense":dense,
     "Conv2D":conv,
+    "Reshape":zero,
+    "InputLayer":zero,
     "Conv2DTranspose":conv_transpose,
     "BatchNormalization": batch_norm,
     "SeparableConv2D": separable_conv,
     "DepthwiseConv2D": depthwise_conv,
+    "Activation": compute_act,
     "ReLU": zero
 }
 
@@ -24,6 +27,7 @@ class Profiler:
         self.flops = 0
         self.table = None
 
+    
 
     def counter(self, mod: models.Model, flag = True):
         if not isinstance(mod, models.Model):
@@ -41,6 +45,7 @@ class Profiler:
                     self.counter(val,False)
                 else:
                     self.table.add_row([key, val.name, "Not Implemented","Not Implemented","Not Implemented"])
+
         if bool(flag):
             print(self.table)
             print("Total Cost  :  " + str(self.flops)+" FLOPs\n")
